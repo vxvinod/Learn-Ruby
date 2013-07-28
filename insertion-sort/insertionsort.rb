0e0b8e28d15eb7285121b1674f547bd6bf1e54bd
vxvinod <vinodkmrmurugan@gmail.com>
2013-07-28 09:33:26 +0530

    insertion sort s done

--- insertion-sort/insertionsort.rb
+++ insertion-sort/insertionsort.rb
@@ -2,30 +2,41 @@ class Insertionsort
 
 $arr=Array.new
 
-def getinput
+	def getinput
 
-	
+		
 
-	puts "Enter the element to be sorted"
-	$a=gets.chomp()
-	while ($a != '/') do
-	puts "To end press /"
-	$arr.push($a)
-	puts "#{$arr} is added to array"
-	$a=gets.chomp()
+		puts "Enter the element to be sorted"
+		$a=gets.chomp()
+		while ($a != '/') do
+		puts "To end press /"
+		$arr.push($a)
+		puts "#{$arr} is added to array"
+		$a=gets.chomp()
+		end
+
 	end
-
-end
 
 	def sort
-	$county=$arr.length
-	for i in 2..$county
-	puts "#{i}i am sort"
+		$county=$arr.length		
+
+		for j in 1..$county-1
+			puts "#{j}"
+			key=$arr[j]
+			i=j-1
+			puts "#{$arr[i]},#{key}"
+			while i>=0 and ($arr[i]>key)  do
+				$arr[i+1]=$arr[i]
+				i=i-1
+			end
+			$arr[i+1]=key
+			puts "#{$arr[j-1]},#{$arr[j]}"
+		end
+		puts $arr
 	end
-	end
-	
 end
 
 ins=Insertionsort.new
 ins.getinput
 ins.sort
+
